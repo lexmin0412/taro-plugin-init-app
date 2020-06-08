3bb0426f64b5ae4df77ea6cab8844d52bd01c07d
张乐 <zhangle@hydee.com>
2020-06-08 17:46:04 +0800

    feat: 小程序端自定义打包页面支持

--- src/getPages.ts
+++ src/getPages.ts
@@ -6,7 +6,7 @@ const fs = require('fs')
 const getPages = (ctx, options) => {
   return new Promise(resolve => {
     const { chalk } = ctx.helper
-    const { homeRoute } = options
+    const { homeRoute, includePages } = options
     console.log(chalk.yellow('开始 '), '进入扫描页面插件')
 
     if (fs.existsSync('./src/pages/routes.js')) {
@@ -37,6 +37,9 @@ const pages = [
             pages.indexOf(`pages/${item}/${sliceRes}`) === -1 &&
             !['component'].includes(sliceRes)
           ) {
+            if (ctx.runOpts.platform === 'weapp' && !includePages.includes(`pages/${item}/${sliceRes}`) ) {
+              return
+            }
             pages.push(`pages/${item}/${sliceRes}`)
           }
         })
