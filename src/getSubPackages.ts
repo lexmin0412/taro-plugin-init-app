5176a9327087c64ac3f99e389a83520468ec9fcd
cathe-zhang <zhangle_media@hotmail.com>
2020-08-18 10:33:20 +0800

    refactor: 分包插件配置支持includes传入

--- src/getSubPackages.ts
+++ src/getSubPackages.ts
@@ -110,64 +110,66 @@ const handlePurifyArr = (weapp, h5, ctx, pages, subPackageItem, path) => {
  */
 const getSubPackages = (ctx, options) => {
   return new Promise(resolve => {
-    const {
-      chalk
-    } = ctx.helper;
-    const {
-      homeRoute,
-      weapp,
-      h5,
-      subPackages: subPackagesConfig
-    } = options;
-    console.log(chalk.yellow('开始 '), '进入扫描分包插件')
-    let excluedeSubPackages = []
+    const {chalk} = ctx.helper;
+    const {homeRoute, weapp, h5, subPackages: subPackagesConfig} = options;
+    console.log(chalk.yellow('开始 '), '进入扫描分包插件');
+    let excluedeSubPackages: any[] = [];
+    let includesSubPackages: any[] = []
     if (subPackagesConfig && subPackagesConfig.excludes) {
-      excluedeSubPackages = subPackagesConfig.excludes
+      excluedeSubPackages = subPackagesConfig.excludes;
     }
-    let indexLines = ''
+    if (subPackagesConfig && subPackagesConfig.includes) {
+      includesSubPackages = subPackagesConfig.includes;
+    }
+    let indexLines = '';
     const subPackages: any[] = [];
     const outerDirs = fs.readdirSync('./src');
+
+    /**
+     * 验证规则
+     */
+    const testFunc = (item: any) => {
+      // 优先判断includes 如果没有includes则判断excludes
+      if (includesSubPackages && includesSubPackages.length) {
+        return includesSubPackages.includes(item)
+      } else if (excluedeSubPackages && excluedeSubPackages.length) {
+        return !['.DS_Store', ...excluedeSubPackages].includes(item)
+      }
+    }
+
     outerDirs.forEach(item => {
       // 跳过特殊文件夹
-      if (!['.DS_Store', ...excluedeSubPackages].includes(item) && isDirectory(`./src/${item}`)) {
-
-        console.log(chalk.magentaBright('读取 '), `发现分包 ${item}`)
-        
-        const subPackageItem: any = {}
-        subPackageItem.root = item
-        subPackageItem.name = item
-        subPackageItem.pages = []
-
+      if (testFunc(item) && isDirectory(`./src/${item}`)) {
+        console.log(chalk.magentaBright('读取 '), `发现分包 ${item}`);
+        const subPackageItem: any = {};
+        subPackageItem.root = item;
+        subPackageItem.name = item;
+        subPackageItem.pages = [];
         const innerDir = fs.readdirSync(`./src/${item}`);
         // 去除后缀名
         innerDir.forEach(inItem => {
+          // 非页面文件夹过滤
           if (['assets', 'components', 'css', 'interface', 'services', 'enums'].includes(inItem)) {
-            return
+            return;
           }
-
           // 如果是文件夹则再次遍历
           if (isDirectory(`./src/${item}/${inItem}`)) {
-
-            const deepInnerDir = fs.readdirSync(`./src/${item}/${inItem}`)
-
+            const deepInnerDir = fs.readdirSync(`./src/${item}/${inItem}`);
             deepInnerDir.forEach(deepInnerItem => {
-
               if (['assets', 'components', 'component', 'css', 'interface', 'services', 'enums'].includes(deepInnerItem)) {
-                return
+                return;
               }
-
               const sliceRes = deepInnerItem.slice(0, deepInnerItem.indexOf('.'));
               // 数组去重
-              handlePurifyArr(weapp, h5, ctx, subPackageItem.pages, item, `${inItem}/${sliceRes}`)
-            })
-
-          } else {
+              handlePurifyArr(weapp, h5, ctx, subPackageItem.pages, item, `${inItem}/${sliceRes}`);
+            });
+          }
+          else {
             const sliceRes = inItem.slice(0, inItem.indexOf('.'));
-            handlePurifyArr(weapp, h5, ctx, subPackageItem.pages, item, `${sliceRes}`)
+            handlePurifyArr(weapp, h5, ctx, subPackageItem.pages, item, `${sliceRes}`);
           }
         });
-
-        subPackages.push(subPackageItem)
+        subPackages.push(subPackageItem);
       }
     });
     subPackages.forEach(item => {
